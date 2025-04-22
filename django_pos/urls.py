856b890cce3d9b284ad67d93479ca5d0cf67ede0
BilalAhmed <ba08018@st.habib.edu.pk>
2025-04-22 14:55:02 +0500

    new

--- django_pos/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    # Include the default password reset URLs from Django's auth system
-    path('accounts/', include('django.contrib.auth.urls')),
-]
